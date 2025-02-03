610ec8040c4852c35eade4e5af67aa525187da3b
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-02 21:45:54 -0400

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,37 +1,37 @@
-import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
+    if (!text) return conn.reply(m.chat, '🔎 Por favor, ingresa una búsqueda de Youtube.', m);
 
-if (!text) return conn.reply(m.chat, `🔎 Por favor, ingresa una busqueda de Youtube.`, m, rcanal, )
+    conn.reply(m.chat, wait, m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: null,
+                mediaType: 1,
+                showAdAttribution: true,
+                title: packname,
+                body: dev,
+                previewType: 0,
+                thumbnail: icons,
+                sourceUrl: channel
+            }
+        }
+    });
 
-conn.reply(m.chat, wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+    try {
+        let results = await yts(text);
+        let tes = results.all;
 
-let results = await yts(text)
-let tes = results.all
-let teks = results.all.map(v => {
-switch (v.type) {
-case 'video': return `「✦」Resultados de la búsqueda para *<${text}>*
+        if (tes.length === 0) return conn.reply(m.chat, 'No se encontraron resultados.', m);
 
-> ☁️ Título » *${v.title}*
-> 🔔 Canal » *${v.author.name}*
-> 🕝 Duración » *${v.timestamp}*
-> 📆 Subido » *${v.ago}*
-> 👀 Vistas » *${v.views}*
-> 🔗 Enlace » ${v.url}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+        let teks = tes.map(v => {
+            if (v.type === 'video') {
+                return `✦ Resultados de la búsqueda para *<${text}>*\n\n> ☁️ Título » *${v.title}*\n> 🔔 Canal » *${v.author.name}*\n> 🕝 Duración » *${v.timestamp}*\n> 📆 Subido » *${v.ago}*\n> 👀 Vistas » *${v.views}*\n> 🔗 Enlace » ${v.url}`;
+            }
+        }).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n');
 
-conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
-
-}
-handler.help = ['ytsearch']
-handler.tags = ['buscador']
-handler.command = ['ytbuscar', 'ytsearch', 'yts']
-
-handler.register = true
-//handler.yenes = 1
-
-export default handler
+        conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m);
+    } catch (error) {
+        console.error(error);
+        conn.reply(m.chat, 'Ocurrió un error al buscar.', m);
+    }
+};
