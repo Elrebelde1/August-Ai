16b877de26865b3c450424deef7932d35f5b2127
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-02 21:48:50 -0400

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,43 +1,19 @@
 import yts from 'yt-search'
-
-var handler = async (m, { text, conn, args, command, usedPrefix }) => {
-
-if (!text) return conn.reply(m.chat, `🌟 *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Inosuke Hashibira`, m, rcanal, )
-
-conn.reply(m.chat, wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-
-let results = await yts(text)
-let tes = results.all
-let teks = results.all.map(v => {
-switch (v.type) {
-case 'video': return `🌟 *Título:* 
-» ${v.title}
-
-🔗 *Enlace:* 
-» ${v.url}
-
-🕝 *Duración:*
-» ${v.timestamp}
-
-🚩 *Subido:* 
-» ${v.ago}
-
-👀 *Vistas:* 
-» ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
-
-conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
-
+let handler = async (m, {conn, text }) => {
+  if (!text) throw `✳️ ${mssg.search('YouTube')}`
+  let results = await yts(text)
+        let tes = results.videos
+let teks = tes.map(v => `
+📌 ${v.title}
+*⌚${mssg.duration}:* ${v.timestamp}
+*📆${mssg.aploud}:* ${v.ago}
+*👀${mssg.views}:* ${v.views.toLocaleString()}
+*🔗${mssg.link}:* ${v.url}
+`.trim()).join('\n________________________\n\n')
+        conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
 }
-handler.help = ['ytsearch']
-handler.tags = ['buscador']
-handler.command = /^playlist|ytbuscar|yts(earch)?$/i
-
-handler.register = true
-handler.estrellas = 1
+handler.help = ['ytsearch'] 
+handler.tags = ['dl']
+handler.command = ['ytsearch', 'yts'] 
 
 export default handler
