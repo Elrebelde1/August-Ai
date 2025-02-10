f7b7ff24690bfe8ab750532cc0c68b630bb47503
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-09 22:14:05 -0400

    Actualizar plugins

---
+++ plugins/_nopor.js
@@ -0,0 +1,18 @@
+
+let handler = async (m) => {
+    const videos = [
+        'https://qu.ax/kZlnj.mp4',
+    ];
+
+    // Elegir un video aleatorio
+    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+    // Enviar el video al chat
+    await conn.sendMessage(m.chat, { video: { url: randomVideo }, caption: "¡Aquí tienes un video para disfrutar!" }, { quoted: m });
+}
+
+handler.help = ['videoxxx'];
+handler.tags = ['diversión'];
+handler.command = ['videoxxx'];
+
+export default handler;
