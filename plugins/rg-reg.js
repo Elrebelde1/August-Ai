d1c8d9ae7e5f3942c4ee2f4877f184237a4a6f2a
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-21 19:22:45 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,35 +1,45 @@
 import { createHash } from 'crypto'
-import fs from 'fs'
-import fetch from 'node-fetch'
-
-let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let Reg = /\|?(.*)([.|+] *?)([0-9]*)([.|+] *?)([MFNO])?$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🧑‍💻 YA ESTAS REGISTRADO.\n\n*¿QUIERES HACERLO DE NUEVO?*\n\nUSE ESTE COMANDO PARA ELIMINAR SU REGISTRO.\n*${usedPrefix}unreg* <Número de serie>`)
-  if (!Reg.test(text)) return m.reply(`⚡ 𝐅𝐎𝐑𝐌𝐀𝐓𝐎 𝐈𝐍𝐂𝐎𝐑𝐄𝐂𝐓𝐎.\n\nUSO 𝐃𝐄𝐋 𝐂𝐎𝐌𝐀𝐍𝐃𝐎: *${usedPrefix + command} 𝑵𝑶𝑴𝑩𝑹𝑬.𝑬𝑫𝑨𝑫*\n𝑬𝑱𝑬𝑴𝑷𝑳𝑶 : *${usedPrefix + command} ${name2}.16*`)
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('👻 𝑬𝑳 𝑵𝑶𝑴𝑩𝑹𝑬 𝑵𝑶 𝑷𝑼𝑬𝑫𝑬 𝑬𝑺𝑻𝑨𝑹 𝑽𝑨𝑪𝑰𝑶.')
-  if (!age) return m.reply('👻 𝑳𝑨 𝑬𝑫𝑨𝑫 𝑵𝑶 𝑷𝑼𝑬𝑫𝑬 𝑬𝑺𝑻𝑨𝑹 𝑽𝑨𝑪𝑰𝑨.')
-  if (name.length >= 100) return m.reply('🫥 𝑬𝑳 𝑵𝑶𝑴𝑩𝑹𝑬 𝑬𝑺𝑻𝑨 𝑴𝑼𝒀 𝑳𝑨𝑹𝑮𝑶.' )
+  let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png')
+ if (user.registered === true) throw `✳️ ${mssg.regIsOn}\n\n${usedPrefix}unreg <sn>`
+
+  let te = `✳️ ${mssg.useCmd}: *${usedPrefix + command} ${mssg.name}+${mssg.age}+${mssg.gender}*\n📌 ${mssg.example}: *${usedPrefix + command}* Fz+17+M\n\n◉ ${mssg.genderList}:\n*- M* = ${mssg.man}\n*- F* ${mssg.woman}\n*- N* = ${mssg.other}`
+  if (!Reg.test(text)) throw te
+  let [_, name, splitter, age, splitter2, gen] = text.match(Reg)
+  if (!name) throw te
+  if (!age) throw te
+  if (name.length >= 30) throw `✳️ ${mssg.nameMax}`
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 𝑾𝑶𝑾 𝑬𝑳 𝑨𝑩𝑼𝑬𝑳𝑶 𝑸𝑼𝑰𝑬𝑹𝑬 𝑱𝑼𝑮𝑨𝑹 𝑨𝑳 𝑩𝑶𝑻.')
-  if (age < 5) return m.reply('🚼 𝑬𝑳 𝑩𝑬𝑩𝑬 𝑸𝑼𝑰𝑬𝑹𝑬 𝑱𝑼𝑮𝑨𝑹 𝑱𝑨𝑱𝑨. ')
+  if (age > 60) throw `👴🏻 ${mssg.oldReg}`
+  if (age < 10) throw '🚼 Vaya a ver la vaca lola'
+  let genStr = gen && gen.toUpperCase() === 'M' ? `🙆🏻‍♂️ ${mssg.man}` : (gen && gen.toUpperCase() === 'F' ? `🤵🏻‍♀️ ${mssg.woman}` : (gen && gen.toUpperCase() === 'N' ? `⚧ ${mssg.other}` : null))
+  if (!genStr) throw `✳️ ${mssg.genderList}: M, F o N\n\n*- M* = ${mssg.man}\n*- F*- ${mssg.woman}\n*- N* = ${mssg.other}`
   user.name = name.trim()
   user.age = age
+  user.genero = genStr
   user.regTime = + new Date
+  user.coin += 8400
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  let img = await (await fetch(`https://qu.ax/OQNSn.jpg`)).buffer()
-  let txt = ` –  *Bienvenido A August-Ai*\n\n`
-      txt += `╔  👤  *NOMBRE* : ${name}\n`
-      txt += `╠  💎  *EDAD* : ${age} años\n`
-await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
-await m.react('✅')
-}
-handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
-handler.tags = ['rg']
+  let regi = `
+┌─「 *${mssg.regOn.toUpperCase()}* 」─
+│ *${mssg.name}:* ${name}
+│ *${mssg.age}:* ${age}
+│ *${mssg.gender}:* ${genStr}
+│ *${mssg.numSn}:*
+${sn}
+└──────────────
 
-handler.command = ['verify', 'reg', 'register', 'registrar'] 
+ \`\`\`⏍ Como bono por tu registro, se te han añadido 8400 coins 🪙 a tu cuenta de banco 🏦\`\`\`
+`
+  conn.sendFile(m.chat, pp, 'img.jpg', regi, m)
+}
+
+handler.help = ['reg'].map(v => v + ' <nombre.edad.género>')
+handler.tags = ['rg']
+handler.command = ['verify', 'reg', 'register', 'registrar', 'verificar'] 
 
 export default handler
