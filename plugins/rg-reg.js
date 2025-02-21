cbe2c810662c3381c3183a6702fa02c7b7799af3
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-21 19:29:59 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,48 +1,45 @@
 
 import { createHash } from 'crypto';
 
-let Reg = /\|?(.*)([.|+] *?)([0-9]*)([.|+] *?)([MFNO])?$/i;
+const Reg = /\|?(.*?)([.|+] *?)(\d+)([.|+] *?)([MFNO])?$/i;
 
-let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let user = global.db.data.users[m.sender];
-  let name2 = conn.getName(m.sender);
-  let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png');
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  const user = global.db.data.users[m.sender];
+  const name2 = conn.getName(m.sender);
+  const pp = await conn.profilePictureUrl(m.sender, 'image').catch(() => './src/avatar_contact.png');
 
-  if (user.registered === true) throw ✳️ ${mssg.regIsOn}\n\n${usedPrefix}unreg <sn>;
+  if (user.registered) throw `✳️ ${mssg.regIsOn}\n\n${usedPrefix}unreg <sn>`;
 
-  let te = ✳️ ${mssg.useCmd}: *${usedPrefix + command} ${mssg.name}+${mssg.age}+${mssg.gender}* 📌 Ejemplo: *${usedPrefix + command}* Fz+17+M\n\n◉ ${mssg.genderList}: *- M* = ${mssg.man}, *- F* = ${mssg.woman}, *- N* = ${mssg.other}`;
+  const te = `✳️ ${mssg.useCmd}: *${usedPrefix + command} ${mssg.name}+${mssg.age}+${mssg.gender}* 📌 Ejemplo: *${usedPrefix + command}* Fz+17+M\n\n◉ ${mssg.genderList}: *- M* = ${mssg.man}, *- F* = ${mssg.woman}, *- N* = ${mssg.other};`;
 
   if (!Reg.test(text)) throw te;
 
-  let [_, name, splitter, age, splitter2, gen] = text.match(Reg);
-  if (!name) throw te;
-  if (!age) throw te;
+  const [_, name, , ageStr, , gen] = text.match(Reg);
+  if (!name || !ageStr) throw te;
 
   name = name.trim();
-  if (name.length >= 30) throw ✳️ ${mssg.nameMax};
+  if (name.length >= 30) throw `✳️ ${mssg.nameMax}`;
 
-  age = parseInt(age);
-  if (age > 60) throw 👴🏻 ${mssg.oldReg};
+  const age = parseInt(ageStr);
+  if (age > 60) throw `👴🏻 ${mssg.oldReg}`;
   if (age < 10) throw '🚼 Vaya a ver la vaca lola';
 
-  let genStr;
-  if (gen) {
-    genStr = gen.toUpperCase() === 'M' ? 🙆🏻‍♂️ ${mssg.man} :
-             gen.toUpperCase() === 'F' ? 🤵🏻‍♀️ ${mssg.woman} :
-             gen.toUpperCase() === 'N' ? ⚧ ${mssg.other} : null;
-  }
+  const genStr = gen ? 
+    gen.toUpperCase() === 'M' ? `🙆🏻‍♂️ ${mssg.man}` :
+    gen.toUpperCase() === 'F' ? `🤵🏻‍♀️ ${mssg.woman}` :
+    gen.toUpperCase() === 'N' ? `⚧ ${mssg.other}` : null : null;
 
-  if (!genStr) throw ✳️ ${mssg.genderList}: M, F o N\n\n*- M* = ${mssg.man}\n*- F* = ${mssg.woman}\n*- N* = ${mssg.other};
+  if (!genStr) throw `✳️ ${mssg.genderList}: M, F o N\n\n*- M* = ${mssg.man}\n*- F* = ${mssg.woman}\n*- N* = ${mssg.other};`;
 
   user.name = name;
   user.age = age;
   user.genero = genStr;
-  user.regTime = +new Date();
+  user.regTime = Date.now();
   user.coin += 8400;
 
-  let sn = createHash('md5').update(m.sender).digest('hex');
+  const sn = createHash('md5').update(m.sender).digest('hex');
 
-  let regi =
+  const regi =
 `┌─「 *${mssg.regOn.toUpperCase()}* 」─
 │ *${mssg.name}:* ${name}
 │ *${mssg.age}:* ${age}
@@ -56,7 +53,7 @@ ${sn}
   conn.sendFile(m.chat, pp, 'img.jpg', regi, m);
 };
 
-handler.help = ['reg'].map(v => v + ' <nombre.edad.género>');
+handler.help = ['reg'].map(v => `${v} <nombre.edad.género>`);
 handler.tags = ['rg'];
 handler.command = ['verify', 'reg', 'register', 'registrar', 'verificar'];
 
