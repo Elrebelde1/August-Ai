7768b3340abf7198e23c3971cba6aab6088d10f3
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-06 18:47:00 -0400

    Create ia-intele.js

---
+++ plugins/ia-intele.js
@@ -0,0 +1,26 @@
+// By Barboza Bot 
+
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+
+if (!text) return m.reply(`*🧑‍💻 Ingresa un texto para generar tu imagen a tu gusto*`);
+
+m.react('🕒');
+await conn.sendMessage(m.chat, {text: '*🧑‍💻 Espere, Estamos Trabajando en su imagen*'}, {quoted: m});
+try {
+const crow = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${text}`;
+
+const texto = `*Resultados De:* ${text}`;
+m.react(rwait);
+await conn.sendMessage(m.chat, { image: { url: crow }, caption: texto }, { quoted: m });
+m.react(done);
+
+} catch (error) {
+console.error(error);
+throw `*🚨 Lo sentimos, ha ocurrido un error 😔*`;
+}
+}
+handler.tags = ['tools'];
+handler.help = ['botbarboza'];
+handler.command = ['botbarboza'];
+
+export default handler;
