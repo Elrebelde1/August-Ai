6eada516fe32a3b5c2e072adee8fdd5efeeef0c4
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-09 00:58:44 -0400

    Actualizar ff-clk.js

--- plugins/ff-clk.js
+++ plugins/ff-clk.js
@@ -3,7 +3,7 @@ import { MessageMedia } from 'whatsapp-web.js';
 
 async function handler(m, { conn }) {
     if (m.body === '.clk') {
-        const media = MessageMedia.fromFilePath('https://i.ibb.co/mCKdjQ73/file.jpg'); // Cambia esta ruta por la imagen que deseas enviar
+        const media = await MessageMedia.fromUrl('https://i.ibb.co/mCKdjQ73/file.jpg'); // URL de la imagen
 
         await conn.sendMessage(m.chat, media, { 
             caption: 'Aquí tienes la imagen que pediste con el comando .clk.' 
