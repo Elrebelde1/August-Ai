c88b1856be0e57cdc7831d3018d7c2dcb8790275
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-09 00:57:05 -0400

    Actualizar ff-clk.js

--- plugins/ff-clk.js
+++ plugins/ff-clk.js
@@ -3,18 +3,16 @@ import { MessageMedia } from 'whatsapp-web.js';
 
 async function handler(m, { conn }) {
     if (m.body === '.clk') {
-        const link = 'https://tu-enlace-aqui.com'; // Agrega tu enlace aquí
-        const media = MessageMedia.fromFilePath('https://i.ibb.co/mCKdjQ73/file.jpg'); // Cambia la ruta a tu imagen
+        const media = MessageMedia.fromFilePath('https://i.ibb.co/mCKdjQ73/file.jpg'); // Cambia esta ruta por la imagen que deseas enviar
 
         await conn.sendMessage(m.chat, media, { 
-            caption: `Aquí tienes el enlace que solicitaste: ${link}` 
+            caption: 'Aquí tienes la imagen que pediste con el comando .clk.' 
         });
     }
 }
 
 handler.help = ['clk'];
-handler.tags = ['ff'];
+handler.tags = ['fun'];
 handler.command = ['clk'];
 
 export default handler;
-```
