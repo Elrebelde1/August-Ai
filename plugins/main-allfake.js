65e11ef306ce22c63cb87fbf67d26fb2082d2e20
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-23 17:29:07 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -37,21 +37,21 @@ let user = global.db.data.users[who]
 let pushname = m.pushName || 'Sin nombre'
 
 //creador y otros
-global.creador = 'Wa.me/584120346669'
+global.creador = 'Wa.me/584146277368'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/584120346669'
-global.namechannel = '♡⃝🚀𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕ᚐ҉ᚐ'
-global.namechannel2 = '✫𝚃𝙴𝙰𝙼  乂 𝚂𝚃𝙰𝚁𝙲𝙾𝚁𝙴✫'
-global.namegrupo = '♡⃝𝓨𝓾𝓴𝓲 𝓢𝓾𝓸𝓾 𝓑𝓸𝓽 𝓞𝓯𝓲𝓬𝓲𝓪𝓵ᚐ҉'
-global.namecomu = '✿ 𝒴𝒰𝒦𝐼 𝒮𝒰𝒪𝒰 𝐵𝒪𝒯 ✿'
-global.colab1 = 'Emma-Violets-Versión'
-global.colab2 = 'Niño Piña'
-global.colab3 = 'Legendary'
+global.asistencia = 'Wa.me/5804146277368'
+global.namechannel = '♡⃝🚀Barboza𝒕ᚐ҉ᚐ'
+global.namechannel2 = 'team Barboza '
+global.namegrupo = '♡⃝Powered Aiᚐ҉'
+global.namecomu = '✿ Powered Barboza ✿'
+global.colab1 = 'Legna'
+global.colab2 = 'deylin'
+global.colab3 = 'siskes'
 
 //Ids channel
-global.idchannel = '120363365444927738@newsletter'
-global.canalIdM = ["120363365444927738@newsletter", "120363365444927738@newsletter"]
-global.canalNombreM = [" 𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒚ᚐ", "✧┊┋◟🚀𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕🚀◞┊┋✧"]
+global.idchannel = '120363414007802886@newsletter'
+global.canalIdM = ["120363414007802886@newsletter", "120363414007802886@newsletter"]
+global.canalNombreM = [" bot Barboza", "✧┊┋◟🚀Barboza🚀◞┊┋✧"]
 global.channelRD = await getRandomChannel()
 
 //Reacciones De Comandos.!
@@ -59,7 +59,7 @@ global.rwait = '🕒'
 global.done = '✅'
 global.error = '✖️'
 
-//Emojis determinado de Yuki Ai
+//Emojis determinado de Barboza Ai
 global.emoji = '🌸'
 global.emoji2 = '🌹'
 global.emoji3 = '🍒'
@@ -73,12 +73,12 @@ global.waittt = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻�
 global.waitttt = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗮 ...*';
 
 //Enlaces
-var canal = 'https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T'  
-let canal2 = 'https://whatsapp.com/channel/0029VavzewJLikg78gILRn1o'
-var git = 'https://github.com/The-King-Destroy' 
-var youtube = 'https://youtube.com/@user-the-king-destroy' 
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
-let correo = 'thekingdestroy507@gmail.com'
+var canal = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'  
+let canal2 = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
+var git = '' 
+var youtube = '' 
+var github = '' 
+let correo = 'sebastianbarboza742@gmail.com'
 
 global.redes = [canal, canal2, git, youtube, github, correo].getRandom()
 
@@ -111,15 +111,15 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [ 
-'https://i.ibb.co/hy3G9Rz/file.jpg',
-'https://i.ibb.co/hy3G9Rz/file.jpg',
-'https://i.ibb.co/hy3G9Rz/file.jpg',
-'https://i.ibb.co/hy3G9Rz/file.jpg',
-'https://i.ibb.co/hy3G9Rz/file.jpg', 
-'https://i.ibb.co/hy3G9Rz/file.jpg'
+'https://qu.ax/LJEVX.jpg')',
+'https://qu.ax/LJEVX.jpg')',
+'https://qu.ax/LJEVX.jpg')',
+'https://qu.ax/LJEVX.jpg')',
+'https://qu.ax/LJEVX.jpg')', 
+'https://qu.ax/LJEVX.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: '🚀 ♡⃝𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕ᚐ҉ᚐ', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: '🚀 ♡⃝Barboza𝒕ᚐ҉ᚐ', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
 
