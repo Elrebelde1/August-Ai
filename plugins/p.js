25b94cf65e6f13c399578ebbb2e5c4595a67b7dc
Wichi <sebastianbarbaro82@gmail.com>
2025-03-30 14:27:34 -0400

    Crear p.js

---
+++ plugins/p.js
@@ -0,0 +1,8 @@
+
+const handler = async (m, { conn }) => {
+  m.reply("el puto más negro del mundo es la zorra prostituta de anuar es un puto negro de mrd ");
+};
+
+handler.command = 'masnegro', /^(masnegro|\.masnegro)$/i;
+
+export default handler;
