a564206f309a06986ae220fba90f28251a1a4ad9
Wichi <sebastianbarbaro82@gmail.com>
2025-03-30 14:33:04 -0400

    Actualizar negro.js

--- plugins/negro.js
+++ plugins/negro.js
@@ -1,6 +1,6 @@
 
 const handler = async (m, { conn }) => {
-  m.reply("el más negro es anuar de .masnegro");
+  m.reply("el mas puto africano y negro es la puta zorra prostituta de Anuar es un puto tremendo gay y africano.");
 };
 
 handler.command = 'africano', /^(africano|\.africano)$/i;
