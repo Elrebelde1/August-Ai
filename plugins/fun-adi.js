ab26cf8a0f09ea10b5898654b8efd6b8f3fd8c12
Wichi <sebastianbarbaro82@gmail.com>
2025-03-31 14:28:56 -0400

    Crear fun-adi.js

---
+++ plugins/fun-adi.js
@@ -0,0 +1,44 @@
+import fs from 'fs'
+
+let timeout = 5000
+let poin = 10000
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+    const apiUrl = 'https://api-kasu.onrender.com/api/game/bandera?apikey=79242cc3';
+    const name = await fetch(apiUrl);
+    const data = await name.json();
+
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
+        throw false
+    }
+    let textos = `
+ⷮ *Adivina el nombre de la bandera de la foto*
+*Nota: Pusimos 2 minutos para poder visualizar la imagen bien ya que esta borrosa, estamos mejorando eso, en muy poco tiempo estara lista con foto hd*
+
+
+*Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*Bono:* +${poin} Exp
+
+Recuerda responder con el nombre completo!
+`.trim()
+    conn.tekateki[id] = [
+
+       conn.sendFile(m.chat, data.img, 'bandera.jpg', textos, m);
+
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo.`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+
+handler.help = ['adivinabandera']
+handler.tags = ['game']
+handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i
+
+export default handler
