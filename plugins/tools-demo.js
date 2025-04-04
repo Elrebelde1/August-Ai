2c2023d7b227cd73d2ed2cf80ea9f09e88874f23
Wichi <sebastianbarbaro82@gmail.com>
2025-04-03 21:11:49 -0400

    Actualizar tools-demo.js

--- plugins/tools-demo.js
+++ plugins/tools-demo.js
@@ -14,7 +14,7 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `[ BarbozaBot -By|Barboza & WillZek ]`,
+title: `[ August -By|Bot & Agust ]`,
 body: ``,
 "previewType": "PHOTO",
 thumbnailUrl: 'https://tinyurl.com/2awg2bch', 
