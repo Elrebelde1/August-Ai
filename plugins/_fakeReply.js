f0b974ce458aaa7bb46ea96997791b8bbf07747a
Wichi <sebastianbarbaro82@gmail.com>
2025-04-07 14:30:24 -0400

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -62,7 +62,7 @@ global.icono = [
     contextInfo: {
             isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363317263885467@newsletter",
+      newsletterJid: "120363375378707428@newsletter",
       serverMessageId: 100,
       newsletterName: name,
     },
